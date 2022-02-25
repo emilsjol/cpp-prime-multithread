fcfbed35acb7af819ea3e5ae1872598a8bec5e54
emilsjol <emil.sjolander11@gmail.com>
2022-02-25 16:44:33 +0100

    findPrimes hittar nu primtal mellan två intervall

--- naive.cpp
+++ naive.cpp
@@ -9,31 +9,27 @@ int naivePrimes(int targetNumber, int numberOfThreads) {
 
 	int n = 0;
 	int temp = 0;
-	std::vector<bool> vector;
+	
+	std::vector<std::thread> threads;
+	
+	std::vector<bool> numbers;
+	
 	std::chrono::time_point<std::chrono::steady_clock> t1, t2, t3, t4;
 	
 	t1 = std::chrono::steady_clock::now();
-	
 	for (int i = 0; i < targetNumber; ++i) {
-		vector.push_back(0);
+		numbers.push_back(0);
 	}
 	
 	t2 = std::chrono::steady_clock::now();
 	
-	while (n <= targetNumber) {
-		
-		if (isPrime(n)) {
-			vector[n] = 1;
-		}
-		
-		n++;
-	}
+	findPrimes(0, targetNumber, numbers);
 	
 	t3 = std::chrono::steady_clock::now();
 	
-	printPrimes(vector);
-	
+	printPrimes(numbers);
 	t4 = std::chrono::steady_clock::now();
+	
 	std::chrono::duration<double> fillEmptyVectorTime = t2 - t1;
 	std::chrono::duration<double> findPrimesTime = t3 - t2;
 	std::chrono::duration<double> printPrimesTime = t4 - t3;
@@ -42,6 +38,21 @@ int naivePrimes(int targetNumber, int numberOfThreads) {
 	std::cout << "printPrimesTime: " << printPrimesTime.count() << std::endl;
 	
 	return 0;
+}
+
+void findPrimes(int startIndex, int endIndex, std::vector<bool> &numbers) {
+	
+	int n = startIndex;
+	
+	while (n < endIndex) {
+		
+		if (isPrime(n)) {
+			numbers[n] = true;
+		}
+		
+		n++;
+	}
+	
 }
 
 bool isPrime(int n) {
@@ -58,7 +69,7 @@ bool isPrime(int n) {
 
 void printPrimes(std::vector<bool> v) {
 	for (int i = 0; i < v.size(); ++i) {
-		if (v[i] == 1) {
+		if (v[i] == true) {
 			std::cout << i << std::endl;
 		}
 	}
--- naive.h
+++ naive.h
@@ -3,3 +3,4 @@
 void printPrimes(std::vector<bool> v);
 bool isPrime(int n);
 int naivePrimes(int targetNumber, int numberOfThreads);
+void findPrimes(int startIndex, int endIndex, std::vector<bool> &numbers);
